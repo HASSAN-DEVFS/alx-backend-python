b3aaca13c46f8ebe3d43e8af261be476f3dffcfa
CYBER_TECH <hdjdjdjxjndj@gmail.com>
2025-09-30 15:21:51 +0100

    projets messaging app

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,9 +1,7 @@
-import uuid
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+import uuid
 
-
-# User model (extension d’AbstractUser)
 class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
@@ -13,6 +11,18 @@ class User(AbstractUser):
         default='guest'
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    # éviter les conflits avec auth.User
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions_set",
+        blank=True
+    )
 
     def __str__(self):
         return self.username
